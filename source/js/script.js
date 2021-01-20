01d15da75fcdaa20430b8e3d2f0410db4043c58f
dyv21 <d.y.vladimirov@gmail.com>
2021-01-20 20:42:50 +0300

    Mob menu

--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,31 @@
 // Menu
-const header = document.querySelector('.page-header')
+const body = document.querySelector('.page-body')
+const header = body.querySelector('.page-header')
 const navMain = header.querySelector('.main-nav');
 const navToggle = navMain.querySelector('.main-nav__toggle');
 
 if (header && navMain && navToggle) {
   header.classList.remove('page-header--nojs');
 
-  navToggle.addEventListener('click',() => {
+  navToggle.addEventListener('click', () => {
     if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
+      openNaMain()
     } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
+      closeNavMain();
     };
   });
+}
+
+const openNaMain = () => {
+  navMain.classList.remove('main-nav--closed');
+  navMain.classList.add('main-nav--opened');
+  body.style = `overflow: hidden;`
+}
+
+const closeNavMain = () => {
+  navMain.classList.add('main-nav--closed');
+  navMain.classList.remove('main-nav--opened');
+  body.removeAttribute('style');
 }
 
 // smooth scroll to anchor
@@ -23,6 +34,11 @@ const anchors = document.querySelectorAll('a[href^="#"]')
 for(let anchor of anchors) {
   anchor.addEventListener("click", (e) => {
     e.preventDefault();
+
+    if (navMain.classList.contains('main-nav--opened')) {
+      closeNavMain();
+    }
+
     const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
     document.querySelector(goto).scrollIntoView({
       behavior: "smooth",
